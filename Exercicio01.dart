136722d3c41b98752d485f543e6a89eec6e40f9a
gabriel.rodrigues <gabrigues23@gmail.com>
2024-12-03 16:45:08 -0300

    upload Exercicio01

--- Exercicio01.dart
+++ Exercicio01.dart
@@ -1,3 +1,21 @@
 void main() {
-  // Facil 1 - Faça um programa que peça dois números, verifique (usando if e else) e imprima o maior deles.
+  // Facil 1 - Faça um programa que peça dois números, verifique (usando if e
+  // else) e imprima o maior deles.
+
+  int num1 = -1;
+  int num2 = 10;
+
+  verificaNumeros().verificar(num1, num2);
+}
+
+class verificaNumeros {
+  void verificar(num1, num2) {
+    if (num1 > num2) {
+      print("O número $num1 é maior que o número $num2.");
+    } else if (num2 > num1) {
+      print("O número $num2 é maior que o número $num1.");
+    } else {
+      print("Os números são iguais.");
+    }
+  }
 }
