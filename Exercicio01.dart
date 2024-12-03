108f1c482e627eb609e3b4e703eff9b20913ab49
GabRodrigues23 <gabrigues23@gmail.com>
2024-12-03 02:12:10 -0300

    upload exercises - not complete

---
+++ Exercicio01.dart
@@ -0,0 +1,3 @@
+void main() {
+  // Facil 1 - Faça um programa que peça dois números, verifique (usando if e else) e imprima o maior deles.
+}
---
+++ Exercicio02.dart
@@ -0,0 +1,3 @@
+void main() {
+  // Intermediário 6 - Faça um programa que leia três números, verifique (usando if e else) e imprima o maior deles.
+}
